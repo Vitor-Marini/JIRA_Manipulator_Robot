ba86a70d13f0a250d2b322e2e8bae4b1d7d97bd4
Vitor <vitorfamrini@gmail.com>
2023-09-10 16:42:52 -0300

    basico do tal servo

--- 5DOF_code/src/main.cpp
+++ 5DOF_code/src/main.cpp
@@ -1,46 +1,43 @@
 #include <Arduino.h>
+#include<ESP32Servo.h>
 
-int  led_verde=12;
-int led_vermelho=25;
-int led_azul=13;
 
-int valor;
-
-//void home_position(int);
+Servo myservo;
 
 void setup() {
-pinMode(led_azul,OUTPUT);
-pinMode(led_vermelho,OUTPUT);
-pinMode(led_verde,OUTPUT);
-Serial.begin(9600);
+myservo.attach(16);
+
 }
 
 void loop() {
-  if(Serial.available()>0){
-    valor=Serial.read();
-  }
-
-
-if(valor=='1'){
-  int valorfinal =valor;
-  digitalWrite(led_azul,HIGH);
-  //delay(20000);
- // home_position(led_azul);
-}
-if(valor=='2'){
-  digitalWrite(led_verde,HIGH);
-  //delay(5000);
-  //home_position(led_verde);
-}
-if(valor=='3'){
-  digitalWrite(led_vermelho,HIGH);
-  //delay(5000);
-  //home_position(led_vermelho);
-}
-
-
+myservo.write(180);
+delay(500);
+myservo.write(0);
+delay(500);
+myservo.write(90);
+delay(500);
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*void home_position(int led){
   digitalWrite(led,LOW);
