63fb847d0475cf8c48bff046a85a4f169eb385e6
Vitor <vitorfamrini@gmail.com>
2023-09-20 00:30:24 -0300

    funciona finalmente

--- 5DOF_code/src/main.cpp
+++ 5DOF_code/src/main.cpp
@@ -2,43 +2,52 @@
 #include<ESP32Servo.h>
 
 
+
+
+
 Servo myservo;
+Servo myservo2;
+Servo myservo3;
+Servo myservo4;
+Servo myservo5;
+Servo myservo6;
+
+
 
 void setup() {
 myservo.attach(16);
+myservo2.attach(17);
+myservo3.attach(18);
+myservo4.attach(19);
+myservo5.attach(21);
+myservo6.attach(15);
 
+/*
+myservo.write(0);
+myservo2.write(0);
+myservo3.write(0);
+myservo4.write(0);
+myservo5.write(0);
+*/
+myservo5.write(0);
+myservo6.write(0);
 }
 
 void loop() {
-myservo.write(180);
-delay(500);
-myservo.write(0);
-delay(500);
-myservo.write(90);
-delay(500);
 
+delay(3000);
+myservo6.write(120);
+delay(3000);
+myservo6.write(0);
+delay(3000);
+myservo5.write(120);
+delay(3000);
+myservo5.write(0);
+delay(1000);
 }
 
 
 
-//test
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*void home_position(int led){
-  digitalWrite(led,LOW);
-} */
