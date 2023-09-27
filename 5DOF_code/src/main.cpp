9e953589f58584ffda0205fdd5f8b22f4768fe24
Vitor <vitorfamrini@gmail.com>
2023-09-27 16:31:44 -0300

    muitos pensamentos, fortes ainda

--- 5DOF_code/src/main.cpp
+++ 5DOF_code/src/main.cpp
@@ -2,52 +2,122 @@
 #include<ESP32Servo.h>
 
 
+void home_pos();
+void blue_pos();
+void pink_pos();
+void gripe_collect_default();
 
 
+void servo_slow_move_BASE(int pos_inicial,int pos_final, const int velocidade);
+void servo_slow_move_GRIPPER(int pos_inicial,int pos_final, const int velocidade);
+void servo_slow_move_GRIPPER_BASE(int pos_inicial,int pos_final, const int velocidade);
+void servo_slow_move_FIRST_ARM(int pos_inicial,int pos_final, const int velocidade);
+void servo_slow_move_SECOND_ARM(int pos_inicial,int pos_final, const int velocidade);
+void servo_slow_move_WRIST(int pos_inicial,int pos_final, const int velocidade);
 
-Servo myservo;
-Servo myservo2;
-Servo myservo3;
-Servo myservo4;
-Servo myservo5;
-Servo myservo6;
-
+Servo servo_base;
+Servo servo_second_arm;
+Servo servo_first_arm;
+Servo servo_gipper_base;
+Servo servo_wrist;
+Servo servo_gripper;
 
 
 void setup() {
-myservo.attach(16);
-myservo2.attach(17);
-myservo3.attach(18);
-myservo4.attach(19);
-myservo5.attach(21);
-myservo6.attach(15);
+servo_base.attach(16);
+servo_first_arm.attach(17);
+servo_second_arm.attach(18);
+servo_gipper_base.attach(19);
+servo_wrist.attach(21);
+servo_gripper.attach(15);
+
+
 
 /*
-myservo.write(0);
-myservo2.write(0);
-myservo3.write(0);
-myservo4.write(0);
-myservo5.write(0);
+servo_gripper.write(0);
+servo_base.write(0);
+servo_first_arm.write(50);
+servo_second_arm.write(0);
+servo_wrist.write(0);
+servo_gipper_base.write(0);
 */
-myservo5.write(0);
-myservo6.write(0);
+
+
+
+
 }
 
 void loop() {
 
-delay(3000);
-myservo6.write(120);
-delay(3000);
-myservo6.write(0);
-delay(3000);
-myservo5.write(120);
-delay(3000);
-myservo5.write(0);
-delay(1000);
+
+
+
+
+servo_slow_move_BASE(0,90,15);
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 
 
 
 
+void servo_slow_move_BASE(int pos_inicial,int pos_final, const int velocidade){
+
+  for (int pos = pos_inicial; pos <= pos_final; pos += 1) {
+    servo_base.write(pos);
+    delay(velocidade); 
+  }
+
+
+
+}
+
+
+void home_pos(){
+servo_gripper.write(0);
+servo_base.write(0);
+servo_first_arm.write(50);
+servo_second_arm.write(0);
+servo_wrist.write(0);
+servo_gipper_base.write(0);
+}
+
+
+
+void blue_pos(){
+void gripe_collect_default();
+
+
+
+void home_pos();
+}
+
+////////////////////////////////////////////////
+
+void pink_pos(){
+void gripe_collect_default();
+
+
+
+void home_pos();
+}
+
+///////////////////////////////////////////////
+
+void gripe_collect_default(){
+
+
+
+}
 
