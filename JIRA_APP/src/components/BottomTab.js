387c77dcb2f98d1bb25d931dcb0ae60028d0783e
GVerso <112283257+GabrielReverso@users.noreply.github.com>
2024-04-08 23:03:52 -0300

    Barra de seleção aprimorada

--- JIRA_APP/src/components/BottomTab.js
+++ JIRA_APP/src/components/BottomTab.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "../screens/Home";
 import Config from "../screens/Config";
@@ -8,24 +8,39 @@ import { StyleSheet } from "react-native";
 const Tab = createBottomTabNavigator();
 
 function MyTabs() {
+
   return (
     <Tab.Navigator
+      initialRouteName="Home"
       screenOptions={{
-        header: () => null,a
         tabBarStyle: styles.barStyle,
-        tabBarLabelStyle: styles.label,
         tabBarShowLabel: false,
+        headerShown: false,
+        tabBarHideOnKeyboard: false,
       }}
     >
+      <Tab.Screen
+        name="Wifi"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+            name={focused ? "wifi" : "wifi-outline"}
+              color={focused ? styles.iconsFocused.color : styles.icons.color}
+              size={focused ? styles.iconsFocused.size : styles.icons.size}
+            />
+          ),
+        }}
+      />
       <Tab.Screen
         name="Home"
         component={Home}
         options={{
           tabBarIcon: ({ focused }) => (
             <Ionicons
-              name="home"
-              color={focused ? "yellow" : styles.icons.color}
-              size={styles.icons.size}
+            name={focused ? "home" : "home-outline"}
+              color={focused ? styles.iconsFocused.color : styles.icons.color}
+              size={focused ? styles.iconsFocused.size : styles.icons.size}
             />
           ),
         }}
@@ -36,9 +51,9 @@ function MyTabs() {
         options={{
           tabBarIcon: ({ focused }) => (
             <Ionicons
-              name="settings"
-              color={focused ? "yellow" : styles.icons.color}
-              size={styles.icons.size}
+              name={focused ? "settings" : "settings-outline"}
+              color={focused ? styles.iconsFocused.color : styles.icons.color}
+              size={focused ? styles.iconsFocused.size : styles.icons.size}
             />
           ),
         }}
@@ -49,16 +64,22 @@ function MyTabs() {
 
 const styles = StyleSheet.create({
   barStyle: {
-    backgroundColor: "#0d1e46",
+    backgroundColor: "#0f2352", //"#0d1e46"
     height: "10%",
+    width: "90%",
+    borderRadius: 20,
+    position: "absolute",
+    borderBlockColor: "#000000ff",
+    bottom: "2.5%",
+    left: "4.5%",
   },
   icons: {
     size: 30,
-    color: "white",
+    color: "#0078a3",
   },
-  label: {
-    fontSize: 15,
-    color: "white",
+  iconsFocused: {
+    size: 38,
+    color: "#00bbff",
   },
 });
 
